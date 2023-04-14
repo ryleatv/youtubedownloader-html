0dcd706c4499ebf5261c85c67e3d3079090cbd1b
ryleatv <98416352+ryleatv@users.noreply.github.com>
2023-04-14 20:58:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ downloadBtn.addEventListener('click', function() {
   downloadLink.href = videoUrl;
   downloadLink.click();
 });
+function downloadVideo() {
+  var link = document.getElementById("videoLink").value;
+  var filename = document.getElementById("videoTitle").textContent;
+  var downloadLink = "https://www.y2mate.com/mates/en68/analyze/ajax";
+
+  var data = {
+    url: link,
+    q_auto: 1,
+    ajax: 1
+  };
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", downloadLink, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+  xhr.responseType = "json";
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      var response = this.response;
+      var downloadUrl = response.result.links[0].url;
+
+      var a = document.createElement("a");
+      a.href = downloadUrl;
+      a.download = filename + ".mp4";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+
+  xhr.send(Object.keys(data).map(function (k) {
+    return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+  }).join("&"));
+}
